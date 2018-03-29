edebf50534845b8a4df45b1a7907d1f1b945641a
rkxie <rkxie@amarsoft.com>
2018-03-29 09:40:53 +0800

    replace var by const or let

--- workforxrk/webgl/js/index.js
+++ workforxrk/webgl/js/index.js
@@ -1,15 +1,15 @@
-var scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material); scene.add(cube);
+const geometry = new THREE.CubeGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material); scene.add(cube);
 camera.position.z = 5;
 function render() {
   requestAnimationFrame(render);
